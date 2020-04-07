6d228c58c314aec9873dd23ada9c6c396a83ddd5
Joel <joel@neutrinographics.com>
2020-04-07 14:03:53 +0700

    disabled infinite loop in test

--- spec/loop_harness_spec.cr
+++ spec/loop_harness_spec.cr
@@ -3,6 +3,6 @@ require "./spec_helper"
 describe RenderLoop::LoopHarness do
   it "creates a loop harness without blowing up" do
     h = RenderLoop::LoopHarness.new(60f64, [TestGame.new.as RenderLoop::Engine])
-    h.start(TestWindow.new)
+    # h.start(TestWindow.new)
   end
 end
